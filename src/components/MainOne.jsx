44504dfd929834beae246998a21071a0066767c7
Noel Sebu <noelsebu555@gmail.com>
2024-10-15 12:11:35 +0530

    alterations done on css hight is now manually set

--- src/components/MainOne.jsx
+++ src/components/MainOne.jsx
@@ -4,9 +4,10 @@ import TravelDetails from "./TravelDetails";
 
 const MainOne = () => {
   return (
-    <div className="flex justify-center ">
-      <div className="h-auto w-[88vw] flex justify-evenly  bg-zinc-800 bg-opacity-70 rounded-3xl pt-9 pb-7">
-        <h1 className="text-[4rem] text-white w-[40vw]">
+    <div className="flex justify-center">
+      <div className="h-[38rem] w-[88vw] flex justify-evenly items-center  bg-black bg-opacity-50 rounded-3xl pt-9 pb-7">
+        {/* height of TravaelDetails is controlling the height of div  */}
+        <h1 className="text-[4.6rem] font-semibold text-white text-center w-[40vw]">
           We Will Find The Best <span className="text-yellow-500">Parking</span> Lots for You
         </h1>
         <TravelDetails />
