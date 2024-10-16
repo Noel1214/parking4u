3354e74bf7318bfdb62ad0ac3f04bc03a64eb75f
Noel Sebu <noelsebu555@gmail.com>
2024-10-16 14:07:16 +0530

    added color to few words in BannerOne UI component

--- src/components/BannerOne.jsx
+++ src/components/BannerOne.jsx
@@ -5,13 +5,13 @@ const BannerOne = () => {
     <div className="h-auto w-[100vw] flex flex-col items-center bg-black">
       <div className="h-[17rem] flex flex-col justify-center text-center  gap-4">
         <h1 className="font-bold text-white text-6xl">
-          YOUR ONE STOP SOLUTION
+          YOUR <span className="text-yellow-500">ONE</span> STOP <span className="text-yellow-500">SOLUTION</span>
         </h1>
         <h4 className="font-semibold text-white text-3xl">
           COMFORTABLE AND EFFICIENT PARKING
         </h4>
         <h4 className="font-semiboldbold text-white text-4xl">
-          THE MODERN WAY FOR CONVENIENT PARKING
+          THE <span className="text-yellow-500">MODERN</span> WAY FOR CONVENIENT <span className="text-yellow-500">PARKING</span>
         </h4>
       </div>
     </div>
