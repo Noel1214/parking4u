42d0c5559fb3dff821d166da7b65374443b18d7a
Noel Sebu <noelsebu555@gmail.com>
2024-10-18 14:31:39 +0530

    width increased for MainOne component and chatIcon hidden by commenting out for now

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,15 +8,15 @@ import { RiChat1Line } from "react-icons/ri";
 const page = () => {
   return (
     <div className={`bg-black `}>
-      <div className={`bg-[url('https://images.pexels.com/photos/1756957/pexels-photo-1756957.jpeg')] bg-cover bg-center bg-fixed h-auto min-h-[100%] w-[100vw] pb-24 flex flex-col gap-14 relative`}>
+      <div className={`bg-[url('https://images.pexels.com/photos/1756957/pexels-photo-1756957.jpeg')] bg-cover bg-center bg-fixed h-auto w-[100vw] pb-24 flex flex-col gap-14 relative`}>
         {/* <Header /> */}
         <Navbar />
         <MainOne />
         <BannerOne /> 
         {/* <PopUpNavBar /> */}
-        <div className='h-12 w-12 rounded-full flex justify-center items-center bg-slate-700 text-white fixed top-[87vh] left-[92vw] scale-90 hover:scale-110 transition-all duration-200 ease-in-out'>
+        {/* <div className='h-12 w-12 rounded-full flex justify-center items-center bg-slate-700 text-white fixed top-[87vh] left-[86vw] scale-90 hover:scale-110 transition-all duration-200 ease-in-out'>
           <RiChat1Line size={30} />
-        </div>
+        </div> */}
       </div>
     </div>
   )
--- src/components/MainOne.jsx
+++ src/components/MainOne.jsx
@@ -4,7 +4,7 @@ import TravelDetails from "./TravelDetails";
 const MainOne = () => {
   return (
     <div className="flex justify-center">
-      <div className="h-[45rem] lg:h-[30rem] w-[88vw] flex flex-col lg:flex-row justify-center lg:justify-evenly items-center bg-black bg-opacity-55 rounded-3xl pt-24 pb-10 lg:pb-9">
+      <div className="h-[45rem] lg:h-[30rem] w-[90vw] flex flex-col lg:flex-row justify-center lg:justify-evenly items-center bg-black bg-opacity-55 rounded-3xl pt-24 pb-10 lg:pb-9">
         {/* height of TravaelDetails is controlling the height of div  */}
         <div className="pt-14">
           <h1 className="text-[1.6rem] lg:text-[4.2rem] font-semibold text-white text-center scale-[1.5] lg:scale-100 w-[40vw] lg:w-[45vw]">
