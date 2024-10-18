052cf9e26bd5eb39932bc3a8787c5080b0c5cdee
Noel Sebu <noelsebu555@gmail.com>
2024-10-18 13:35:40 +0530

    TravelDetails component made resopnsive and size scalled down from small screens, borders also added

--- src/components/TravelDetails.jsx
+++ src/components/TravelDetails.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const TravelDetails = () => {
   return (
-    <div className="h-[32rem] w-[27rem] bg-black bg-opacity-55 rounded-3xl scale-90 hover:scale-100 overflow-hidden transition-all duration-300 ease-in-out">
+    <div className="h-[32rem] w-[27rem] border-[0.5px] border-zinc-500 bg-black bg-opacity-55 rounded-3xl scale-75 lg:scale-[0.85] lg:hover:scale-90 overflow-hidden transition-all duration-300 ease-in-out">
       <div className="h-[4rem] bg-zinc-400 flex justify-center items-center bg-opacity-30">
         <h1 className="font-semibold text-3xl text-white">GET A QUOTE</h1>
       </div>
@@ -10,13 +10,19 @@ const TravelDetails = () => {
         {/* From Location  */}
         <div className="flex flex-col gap-2">
           <h1 className="text-white font-semibold text-sm">Traveling From</h1>
-          <input type="text" className="h-10 w-[25rem] p-2 outline-none rounded-md" />
+          <input
+            type="text"
+            className="h-10 w-[25rem] p-2 outline-none rounded-md"
+          />
         </div>
         {/* Drop Off Date Time  */}
         <div className="flex gap-2 w-[25rem] justify-between">
           <div>
             <h1 className="text-white font-semibold text-sm">Drop Off Date</h1>
-            <input type="date" className="h-10 w-[12rem] p-2 outline-none rounded-md" />
+            <input
+              type="date"
+              className="h-10 w-[12rem] p-2 outline-none rounded-md"
+            />
           </div>
           <div>
             <h1 className="text-white font-semibold text-sm">Time</h1>
@@ -31,7 +37,10 @@ const TravelDetails = () => {
         <div className="flex gap-2 w-[25rem] justify-between">
           <div>
             <h1 className="text-white font-semibold text-sm">Pick Up Date</h1>
-            <input type="date" className="h-10 w-[12rem] p-2 outline-none rounded-md" />
+            <input
+              type="date"
+              className="h-10 w-[12rem] p-2 outline-none rounded-md"
+            />
           </div>
           <div>
             <h1 className="text-white font-semibold text-sm">Time</h1>
@@ -45,11 +54,17 @@ const TravelDetails = () => {
         {/* Promo Code  */}
         <div className="flex flex-col gap-2">
           <h1 className="text-white font-semibold text-sm">Promo Code</h1>
-          <input type="text" className="h-10 w-[25rem] p-2 outline-none rounded-md" />
+          <input
+            type="text"
+            className="h-10 w-[25rem] p-2 outline-none rounded-md"
+          />
         </div>
         {/* Submit Details  */}
         <div className="">
-          <button className="h-10 w-[25rem] rounded-md text-white font-semibold text-sm bg-zinc-400 mt-6 bg-opacity-35 hover:bg-opacity-65 transition-all ease-in-out duration-200"> Find Parking</button>
+          <button className="h-10 w-[25rem] rounded-md text-white font-semibold text-sm bg-zinc-400 mt-6 bg-opacity-35 hover:bg-opacity-65 transition-all ease-in-out duration-200">
+            {" "}
+            Find Parking
+          </button>
         </div>
       </form>
     </div>
@@ -57,5 +72,3 @@ const TravelDetails = () => {
 };
 
 export default TravelDetails;
-
-
