4528fe850af2d37126ab2cfad0f9ea9f21bb4fb9
Noel Sebu <noelsebu555@gmail.com>
2024-10-18 13:38:18 +0530

    different "flex" properties for small screens and large screen added, more alterations made for responsiveness

--- src/components/MainOne.jsx
+++ src/components/MainOne.jsx
@@ -4,12 +4,17 @@ import TravelDetails from "./TravelDetails";
 const MainOne = () => {
   return (
     <div className="flex justify-center">
-      <div className="h-[38rem] w-[88vw] flex justify-evenly items-center  bg-black bg-opacity-50 rounded-3xl pt-9 pb-7">
+      <div className="h-[45rem] lg:h-[30rem] w-[88vw] flex flex-col lg:flex-row justify-center lg:justify-evenly items-center bg-black bg-opacity-55 rounded-3xl pt-24 pb-10 lg:pb-9">
         {/* height of TravaelDetails is controlling the height of div  */}
-        <h1 className="text-[4.6rem] font-semibold text-white text-center w-[40vw]">
-          We Will Find The Best <span className="text-yellow-500">Parking</span> Lots for You
-        </h1>
-        <TravelDetails />
+        <div className="pt-14">
+          <h1 className="text-[1.6rem] lg:text-[4.2rem] font-semibold text-white text-center scale-[1.5] lg:scale-100 w-[40vw] lg:w-[45vw]">
+            We Will Find The Best{" "}
+            <span className="text-yellow-500">Parking</span> Lots for You
+          </h1>
+        </div>
+        <div className="mb-4">
+          <TravelDetails />
+        </div>
       </div>
     </div>
   );
