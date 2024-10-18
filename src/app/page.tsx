6c4743f7fbf8f0917c30a8df7381543befe0989a
Noel Sebu <noelsebu555@gmail.com>
2024-10-18 14:12:29 +0530

    background black added for the website in pages file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,12 +7,13 @@ import { RiChat1Line } from "react-icons/ri";
 
 const page = () => {
   return (
-    <div>
-      <div className={`bg-[url('https://images.pexels.com/photos/1756957/pexels-photo-1756957.jpeg')] bg-cover bg-center bg-fixed h-[100vh] w-[100vw] pb-10 flex flex-col gap-14 overflow-x-hidden relative`}>
+    <div className={`bg-black `}>
+      <div className={`bg-[url('https://images.pexels.com/photos/1756957/pexels-photo-1756957.jpeg')] bg-cover bg-center bg-fixed h-auto min-h-[100%] w-[100vw] pb-24 flex flex-col gap-14 relative`}>
         {/* <Header /> */}
         <Navbar />
         <MainOne />
-        <BannerOne />
+        <BannerOne /> 
+        {/* <PopUpNavBar /> */}
         <div className='h-12 w-12 rounded-full flex justify-center items-center bg-slate-700 text-white fixed top-[87vh] left-[92vw] scale-90 hover:scale-110 transition-all duration-200 ease-in-out'>
           <RiChat1Line size={30} />
         </div>
