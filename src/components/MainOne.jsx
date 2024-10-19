7de49e14882a23a22655290d2f57de77f06db44e
Noel Sebu <noelsebu555@gmail.com>
2024-10-19 08:11:01 +0530

    height increased for main one padding for contents removed Text and TravelDetails

--- src/components/MainOne.jsx
+++ src/components/MainOne.jsx
@@ -4,9 +4,9 @@ import TravelDetails from "./TravelDetails";
 const MainOne = () => {
   return (
     <div className="flex justify-center">
-      <div className="h-[45rem] lg:h-[30rem] w-[90vw] flex flex-col lg:flex-row justify-center lg:justify-evenly items-center bg-black bg-opacity-55 rounded-3xl pt-24 pb-10 lg:pb-9">
+      <div className="h-[45rem] lg:h-[35rem] w-[90vw] flex flex-col lg:flex-row justify-center lg:justify-evenly items-center bg-black bg-opacity-55 rounded-3xl pt-24 pb-10 lg:pb-9">
         {/* height of TravaelDetails is controlling the height of div  */}
-        <div className="pt-14">
+        <div className="">
           <h1 className="text-[1.6rem] lg:text-[4.2rem] font-semibold text-white text-center scale-[1.5] lg:scale-100 w-[40vw] lg:w-[45vw]">
             We Will Find The Best{" "}
             <span className="text-yellow-500">Parking</span> Lots for You
