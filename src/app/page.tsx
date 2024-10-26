561f08391274b88e7de97adc7a1cbf5a8669e4b1
Noel Sebu <noelsebu555@gmail.com>
2024-10-26 17:13:34 +0530

    imported and injected New/NavBar component in page.tsx file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,15 @@ import MainOne from '@/components/MainOne';
 import BannerOne from '@/components/BannerOne';
 import InfoCard from '@/components/InfoCard';
 import OurServices from '@/components/OurServices';
+import NewNavigationBar from '@/components/NewNavigationBar';
 
 const page = () => {
   return (
     <div className={`bg-black `}>
       <div className={`bg-[url('https://images.pexels.com/photos/1756957/pexels-photo-1756957.jpeg')] bg-cover bg-center bg-fixed h-auto w-[100vw] pb-24 flex flex-col gap-14 relative`}>
         {/* <Header /> */}
-        <Navbar />
+        {/* <Navbar /> */}
+        <NewNavigationBar />
         <MainOne />
         <BannerOne />
         {/* <PopUpNavBar /> */}
